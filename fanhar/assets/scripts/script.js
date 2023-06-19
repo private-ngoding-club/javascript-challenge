44ed197f1bcf17d78a4f2affa38add7b964965c5
westtle <cuberkuber@gmail.com>
2023-06-20 06:17:35 +0700

    feat: select things needed in javascript

---
+++ fanhar/assets/scripts/script.js
@@ -0,0 +1,6 @@
+// HTML.
+const timeLeft = document.querySelector(".timer__left");
+const yourHand = document.querySelector(".hand__you img");
+const computerHand = document.querySelector(".hand__computer img");
+const choiceButtons = document.querySelectorAll(".choice-box-thingy button");
+const opponentChoosingMessage = document.querySelector(".choice__opponent-message");
