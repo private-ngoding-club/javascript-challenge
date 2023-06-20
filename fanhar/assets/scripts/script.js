c7e48df2b85c8ba4583c39d6a42647e0de228c74
westtle <cuberkuber@gmail.com>
2023-06-20 07:55:55 +0700

    style: change class name for buttons container

--- fanhar/assets/scripts/script.js
+++ fanhar/assets/scripts/script.js
@@ -4,7 +4,7 @@ let gameTimer;
 const timeLeft = document.querySelector(".timer__left");
 const yourHand = document.querySelector(".hand__you img");
 const computerHand = document.querySelector(".hand__computer img");
-const choiceButtons = document.querySelectorAll(".choice-box-thingy button");
+const choiceButtons = document.querySelectorAll(".choice__box-thingy button");
 const opponentChoosingMessage = document.querySelector(".choice__opponent-message");
 
 function selectChoice() {
